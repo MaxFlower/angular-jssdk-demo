66ae9d1152f9fd4d251f0ee2aa6843756144be16
Maksim Tsvetkov <10515741+MaxFlower@users.noreply.github.com>
2020-12-18 11:22:27 +0200

    feat: add js-sdk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import Veriff from '@veriff/js-sdk';
+
+const VRF_PARAMS = {
+  apiKey: 'API_KEY',
+  parentId: 'veriff-root',
+  onSession: (err, response) => {
+    console.log(err, response);
+  }
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-jssdk';
+export class AppComponent implements OnInit {
+  title = 'sdk-check';
+  private veriff: Veriff;
+
+  ngOnInit(): void {
+    this.mountVeriff();
+  }
+
+  private mountVeriff(): void {
+    this.veriff = Veriff(VRF_PARAMS);
+    this.veriff.mount();
+  }
 }
